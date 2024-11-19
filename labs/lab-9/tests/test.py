e87adf3eaa380ecbc7b00ffa5345c6edda3bc7b5
riandickman <147005520+riandickman@users.noreply.github.com>
2024-11-19 16:33:13 -0500

    completed lab 9

--- labs/lab-9/tests/test.py
+++ labs/lab-9/tests/test.py
@@ -24,3 +24,31 @@ def test_insert_user(db_session, sample_signup_input):
 
     assert selected_user is not None
     assert selected_user.LastName == "Phippen"
+
+def test_login(db_session, sample_signup_input):
+    insert_stmt = insert(User).values(sample_signup_input)
+
+    # execute insert query
+    db_session.execute(insert_stmt)
+    # commit the changes to the db
+    db_session.commit()
+
+    # not part of the app.py code, just being used to get the inserted data
+    selected_user = db_session.query(User).filter_by(FirstName="Calista").first()
+
+    assert selected_user is not None
+    assert selected_user.LastName == "Phippen"
+
+def test_sample_data(sample_data):
+    assert sample_data["name"] == "Rian"
+    assert sample_data["age"] == 18
+    assert sample_data["email"] == "rian@rian.com"
+
+def test_failure(sample_data):
+    assert sample_data["name"] == "Rain"
+
+def test_success(sample_data):
+    assert sample_data["email"] == "rian@rian.com"
+
+def test_database_connection(database_connection):
+    pass
