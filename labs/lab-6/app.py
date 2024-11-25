b3fdececc6a710c64f51a0d9055861a6af9e5f72
riandickman <147005520+riandickman@users.noreply.github.com>
2024-11-24 19:03:41 -0500

    update lab 6

--- labs/lab-6/app.py
+++ labs/lab-6/app.py
@@ -2,6 +2,15 @@ from flask import Flask, request, render_template, redirect, url_for, session
 from sqlalchemy import select
 from db.server import db  
 from db.schema.user import User 
+from flask import request, render_template, redirect, url_for
+from sqlalchemy import insert, text, select
+
+from db.server import app
+from db.server import db
+
+from db.schema.user import User
+
+from socketserver import *
 
 app = Flask(__name__)
 
@@ -21,58 +30,71 @@ with app.app_context():
 def index():
     return render_template('index.html')
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        email = request.form['email']
-        password = request.form['password']
-
-  
-        existing_user = db.session.execute(select(User).where(User.email == email)).scalar_one_or_none()
-        if existing_user:
-            return redirect(url_for('signup'))  
-
-  
-        new_user = User(
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            password=password 
-        )
-        db.session.add(new_user)
-        db.session.commit()
-
-        return redirect(url_for('login'))
-
-    return render_template('signup.html')
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    user_error_msg = ""
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+        if "email" in request.form and "password" in request.form:
+            user = db.session.execute(select(User).where(User.email == request.form['email'])).scalar_one_or_none()
+            if user and user.password == request.form['password']:
+                session['user_id'] = user.id
+                session['email'] = user.email
+                return redirect(url_for('index'))
+            else:
+                user_error_msg = "Invalid credentials :("
 
+    return render_template('login.html', error=user_error_msg)
 
-        user = db.session.execute(select(User).where(User.email == email)).scalar_one_or_none()
-
-        if user and user.password == password:
-            session['user_id'] = user.id
-            session['email'] = user.email
-            return redirect(url_for('index')) 
-        else:
-            return redirect(url_for('login'))
-
-    return render_template('login.html')
 
 @app.route('/users')
 def users():
-    with app.app_context():
+    all_users = db.session.execute(select(User)).scalars().all()
+    
 
-        stmt = select(User)
-        all_users = db.session.execute(stmt).scalars().all()  
-        return render_template('users.html', users=all_users)
+    return render_template('users.html', users=all_users)
 
-if __name__ == "__main__":
+
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    print(request.method)
+    if request.method == 'POST':
+       
+        for key, value in request.form.items():
+             print(f'{key}: {value}')
+        query = insert(User).values(request.form)
+
+        with app.app_context():
+                db.session.execute(query)
+                db.session.commit()
+
+        return redirect (url_for ("loginpage"))
+
+
+    return render_template('signup.html')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def loginpage():
+    user_error_msg = ""
+    if request.method == 'POST':
+
+        if "Email" in request.form and "Password" in request.form:
+        
+            stmt = select(User.Password).where(User.Email == request.form['Email'])
+            user = db.session.execute(stmt).fetchone()
+            print(user)
+            print(request.form['Password'])
+
+            if user[0] == request.form['Password']:
+                return redirect(url_for('home'))
+        
+            else: 
+                user_error_msg = "invalid credentials :("
+                return render_template('loginpage.html', error = user_error_msg)
+             
+    return render_template('loginpage.html', error = user_error_msg)
+
+if __name__ == '__main__':
     app.run(debug=True)
+
